1362b7a96a56fdcae153da3dbf3f45c1159868a6
Alexander Barnard <c9ed0316fa@protonmail.com>
2024-03-12 06:36:03 -0400

    Added unit tests

--- solana-twitter/tests/solana-twitter.ts
+++ solana-twitter/tests/solana-twitter.ts
@@ -1,4 +1,5 @@
 import * as anchor from "@coral-xyz/anchor";
+import { AnchorError } from "@coral-xyz/anchor";
 import { assert } from "chai";
 
 describe("solana-twitter", () => {
@@ -24,9 +25,112 @@ describe("solana-twitter", () => {
 
     // After sending the transaction to the blockchain.// Fetch the account details of the created tweet.
     const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
-    console.log(tweetAccount);
+    // console.log(tweetAccount);
 
-    assert.ok(tweetAccount.author.equals(program.provider.publicKey));
+    assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, 'Solana is fast');
+    assert.equal(tweetAccount.content, 'People don\'t like to use slow software. #solana');
+    assert.ok(tweetAccount.timestamp);
+  });
+
+  it('can send a new tweet without a topic', async () => {
+    // Call the "SendTweet" instruction.
+    const tweet = anchor.web3.Keypair.generate();
+    await program.methods
+      .sendTweet('', 'coffee?')
+      .accounts({
+        tweet: tweet.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([tweet])
+      .rpc()
+
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+    // Ensure it has the right data.
+    assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, '');
+    assert.equal(tweetAccount.content, 'coffee?');
+    assert.ok(tweetAccount.timestamp);
+  });
+
+  it('can send a new tweet from a different author', async () => {
+    // Generate another user and airdrop them some SOL.
+    const otherUser = anchor.web3.Keypair.generate();
+    // Airdrop some lamports to the other user to pay rent for the tweet
+    const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature);
+
+    // Call the "SendTweet" instruction on behalf of this other user.
+    const tweet = anchor.web3.Keypair.generate();
+    await program.methods
+      .sendTweet('Someone else', 'Published from another author')
+      .accounts({
+        tweet: tweet.publicKey,
+        author: otherUser.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([otherUser, tweet])
+      .rpc()
+
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+    // Ensure it has the right data.
+    assert.equal(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, 'Someone else');
+    assert.equal(tweetAccount.content, 'Published from another author');
+    assert.ok(tweetAccount.timestamp);
+  });
+
+  it('cannot provide a topic with more than 50 characters', async () => {
+    const tweet = anchor.web3.Keypair.generate();
+    const topicWith51Chars = 'x'.repeat(51); // 51 characters long
+    try {
+      await program.methods
+        .sendTweet(topicWith51Chars, 'some tweet body')
+        .accounts({
+          tweet: tweet.publicKey,
+          author: program.provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([tweet])
+        .rpc()
+    } catch (_err) {
+      assert.isTrue(_err instanceof AnchorError);
+      const err: AnchorError = _err;
+      const errMsg = 'The provided topic should be 50 characters long maximum.';
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      assert.strictEqual(err.error.errorCode.number, 6000);
+      return;
+    }
+    assert.fail('The instruction should have failed with a 51-character topic.');
+  });
+
+  it('cannot provide content with more than 280 characters', async () => {
+    const tweet = anchor.web3.Keypair.generate();
+    const contentWith281Chars = 'x'.repeat(281); // 51 characters long
+    try {
+      await program.methods
+        .sendTweet('Some topic', contentWith281Chars)
+        .accounts({
+          tweet: tweet.publicKey,
+          author: program.provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([tweet])
+        .rpc()
+    } catch (_err) {
+      assert.isTrue(_err instanceof AnchorError);
+      const err: AnchorError = _err;
+      const errMsg = 'The provided content should be 280 characters long maximum.';
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      assert.strictEqual(err.error.errorCode.number, 6001);
+      return;
+    }
+    assert.fail('The instruction should have failed with a 281-character content.');
   });
 
 });
