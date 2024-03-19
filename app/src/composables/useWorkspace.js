5b670a9f6146df443a6d207d171e5dc14c9d9ec3
Alexander Barnard <c9ed0316fa@protonmail.com>
2024-03-19 18:40:19 +0700

    Cleaned up workspace code

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -3,21 +3,22 @@
  */
 
 import { computed } from 'vue'
-import { useAnchorWallet } from 'solana-wallets-vue'
-import { Connection, PublicKey } from '@solana/web3.js'
-import { Provider, Program } from '@project-serum/anchor'
-import idl from '../../../solana-twitter/target/idl/solana_twitter.json'
+import { useAnchorWallet } from 'solana-wallets-vue';
+import { Connection, PublicKey } from '@solana/web3.js';
+import { AnchorProvider, Program } from '@project-serum/anchor';
+// import { AnchorProvider } from '@project-serum/anchor';
+import idl from '../../../solana-twitter/target/idl/solana_twitter.json';
 
-const programID = new PublicKey(idl.metadata.address)
-let workspace = null
+const programID = new PublicKey(idl.metadata.address);
+let workspace = null;
 
-export const useWorkspace = () => workspace
+export const useWorkspace = () => workspace;
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet();
     const connection = new Connection('http://127.0.0.1:8899');
     // Mutate Provider when wallet or connection changes
-    const provider = computed(() => new Provider(connection, wallet.value)); 
+    const provider = computed(() => new AnchorProvider(connection, wallet.value)); 
     // Mutate Program when provider changes
     const program = computed(() => new Program(idl, programID, provider.value));
 
@@ -27,5 +28,5 @@ export const initWorkspace = () => {
         connection,
         provider,
         program,
-    }
+    };
 }
