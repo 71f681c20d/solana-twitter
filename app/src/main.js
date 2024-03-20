23bf8d88fa39a0c7cfe6de8ac7ba12f77105ce6e
Alexander Barnard <c9ed0316fa@protonmail.com>
2024-03-20 09:08:24 +0700

    Connected frontend and backend; Displays tweets via anchor provider

--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,13 @@
 import 'solana-wallets-vue/styles.css'
 import './main.css'
 
+// Day.js for date formatting
+import dayjs from 'dayjs'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(localizedFormat)
+dayjs.extend(relativeTime)
+
 // Router configuration
 import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from './routes'
